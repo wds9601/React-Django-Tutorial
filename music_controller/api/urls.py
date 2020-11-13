92963cf41812f513ae21f08d7f40ed21b595020e
William Downey-Schell <Will@Williams-MacBook-Air.local>
2020-11-13 12:20:40 -0800

    initial commit, beginning setup of Django api, home route stubbed

---
+++ music_controller/api/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from .views import main
+
+# this file controls the url endpoints, sending the app the proper file
+# when the url endpint is hit
+
+urlpatterns = [
+    path('', main)  # since argument is blank string = "domain.com/..."
+                    # when any url endpint is hit, dispatch to views and run "main" function
+]
