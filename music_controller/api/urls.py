96fa9e0409c3093d57324ba3f25524eb6b7c7a64
William Downey-Schell <Will@Williams-MacBook-Air.local>
2020-11-14 17:03:49 -0800

    created frontend folder, npm init, installed webpack and babel

--- music_controller/api/urls.py
+++ music_controller/api/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import main
+from .views import RoomView
 
 # this file controls the url endpoints, sending the app the proper file
 # when the url endpint is hit
 
 urlpatterns = [
-    path('', main)  # since argument is blank string = "domain.com/..."
+    # path('', main)  # since argument is blank string = "domain.com/..."
                     # when any url endpint is hit, dispatch to views and run "main" function
+    path('room', RoomView.as_view()),
 ]
--- music_controller/api/views.py
+++ music_controller/api/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from rest_framework import generics
+from .models import Room
+from .serializers import RoomSerializer
+# from django.http import HttpResponse
 
 # Create your views here.
 
-def main(request):
-    return HttpResponse("<h1>Hello</h1>")
+# def main(request):
+#     return HttpResponse("<h1>Hello</h1>")
+
+class RoomView(generics.CreateAPIView):
+    queryset = Room.objects.all()
+    serializer_class = RoomSerializer
