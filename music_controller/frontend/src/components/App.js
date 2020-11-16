f7ead88597e197af795c9dec5c2dc5accb845f44
William Downey-Schell <Will@Williams-MacBook-Air.local>
2020-11-16 12:58:09 -0800

    added index.css and basic formatting, created stub components for Home, Join, Create, imported react router and added urls to django

--- music_controller/frontend/src/components/App.js
+++ music_controller/frontend/src/components/App.js
@@ -1,13 +1,22 @@
 import React, { Component } from "react";
 import {render} from "react-dom";
+import HomePage from "./HomePage";
+import JoinRoomPage from "./JoinRoomPage";
+import CreateRoomPage from "./CreateRoomPage";
+
+
 
 export default class App extends Component {
     constructor(props) {
         super(props);
     }
-    
+
     render() {
-        return <h1>Testing React Frontend</h1>
+        return (
+        <div>
+            <HomePage />
+        </div>
+        )
     }
 }
 
---
+++ music_controller/frontend/src/components/HomePage.js
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+import JoinRoomPage from "./JoinRoomPage";
+import CreateRoomPage from "./CreateRoomPage";
+import {BrowserRouter as Router, Route, Link, Redirect, Switch} from "react-router-dom";
+
+
+export default class HomePage extends Component {
+    constructor(props) {
+        super(props)
+    }
+    render() {
+        return (
+            <Router>
+                <Switch>
+                    <Route exact path="/"><p>This is the Home Page.</p></Route>
+                    <Route path="/join" component={JoinRoomPage} />
+                    <Route path="/create" component={CreateRoomPage} />
+                </Switch>
+            </Router>    
+        )
+    }
+}
