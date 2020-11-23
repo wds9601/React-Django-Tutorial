16195f9e6c846186882491ba6a68c2949aad6c5c
William Downey-Schell <Will@Williams-MacBook-Air.local>
2020-11-22 20:54:41 -0800

    added POST route to create room, updated api serialzers, views, urls as needed

--- music_controller/api/urls.py
+++ music_controller/api/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import RoomView
+from .views import RoomView, CreateRoomView
 
 # this file controls the url endpoints, sending the app the proper file
 # when the url endpint is hit
@@ -8,4 +8,5 @@ urlpatterns = [
     # path('', main)  # since argument is blank string = "domain.com/..."
                     # when any url endpint is hit, dispatch to views and run "main" function
     path('room', RoomView.as_view()),
+    path('create-room', CreateRoomView.as_view())
 ]
